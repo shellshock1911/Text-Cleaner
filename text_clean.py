36249d90a27c9702aeaac9c7d0490ea007d2d525
Franklin Bradfield <fbradfield1@gmail.com>
2017-08-31 15:16:29 +0800

    Initial commit

---
+++ text_clean.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 15:14:57 2017
+
+@author: BennyBluebird
+"""
+
---
+++ text_clean_utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 15:15:25 2017
+
+@author: BennyBluebird
+"""
+
