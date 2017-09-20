458497b472db2724e571621fd598a6be25468a0e
Franklin Bradfield <fbradfield1@gmail.com>
2017-09-20 09:29:50 -0400

    Ship version 1.1

--- text_clean_utils.py
+++ text_clean_utils.py
@@ -2,7 +2,7 @@
 
 """Set of helper functions for text_clean.py"""
 
-# Allow compatibility between Python 2.7 and 3.5
+# Allow compatibility between Python 2.7 and 3.x
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -300,10 +300,13 @@ def _remove_gibberish(cleaned_message):
             cleaned_message[i] = ''
             
     # Keep tokens that were not erased
-    final_message = [tok for tok in cleaned_message if tok != '']           
-                
+    final_message = [tok for tok in cleaned_message if tok != '']
+    # Return empty if all that is left is random punctuation and symbols
+    if all(char in PUNCTUATION or char in SYMBOLS for char in ''.join(final_message)):
+        return ''
+    else:           
     # Return tokens that were kept
-    return final_message
+        return final_message
 
 
 def _clean_message(message):
@@ -373,16 +376,17 @@ def clean_text(current_input):
     messages = _read_input(current_input)
     # Iteratively add new messages as they are generated
     cleaned_messages = []
+    # Loop over each message
     for message in messages:
+        # Append empty if message contains no content
         if not message:
-        # If message contains no content, append empty message
             cleaned_message = ''
-        # Otherwise, attempt cleaning the message
         else:
+        # Attempt cleaning the message
             cleaned_message = _clean_message(message)
-        # Append each new cleaned message to the list of cleaned messages
+        # Append each cleaned message to the list of cleaned messages
         cleaned_messages.append(' '.join(_join_punctuation(cleaned_message)))            
-    # Join each message with a divider for final output
+    # Join each message with a pipe divider for final output
     cleaned_messages = ' | '.join(cleaned_messages)
             
     # Return cleaned messages string to write to new file
